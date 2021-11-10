631d79a83f7c8b55ed10c73f83d8e1acfb47e93f
jlian2 <jlian2@jlian2s-MacBook-Pro.local>
2021-11-10 00:13:32 -0800

    finish encoder and decoder for ae_csnmf

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import torch
 import torch.nn as nn
 from dataloader import EMA_Dataset
-from models.csnmf import CSNMF
+from models.csnmf import CSNMF,AE_CSNMF
 
 ema_dataset = EMA_Dataset()     
-ema_dataloader = torch.utils.data.DataLoader(dataset=ema_dataset, batch_size=1, shuffle=True)
-model = CSNMF()
+ema_dataloader = torch.utils.data.DataLoader(dataset=ema_dataset, batch_size=16, shuffle=True)
+model = AE_CSNMF()
 
 for i, ema in enumerate(ema_dataloader):
     #print(ema.shape) #[1,t=500,12]
--- src/models/csnmf.py
+++ src/models/csnmf.py
@@ -33,6 +33,7 @@ class AE_CSNMF(nn.Module):
         self.t = 500
         self.num_pellets = 12
         self.num_gestures = 100
+        self.conv_encoder = nn.ConvTranspose2d(in_channels=self.num_pellets,out_channels=1,kernel_size=(self.num_gestures,self.win_size),padding=0)
         self.conv_decoder = nn.Conv2d(in_channels=1,out_channels=self.num_pellets,kernel_size=(self.num_gestures,self.win_size),padding=0)
 
 
@@ -41,9 +42,20 @@ class AE_CSNMF(nn.Module):
         inp = inp.transpose(-1, -2) #[B, num_pellets, t]
         batch_size = inp.shape[0]
 
-        H = torch.randn(batch_size,1,self.num_gestures, self.t)
+        
+        #print("before unsqueeze, inp_shape",inp.shape)
+        inp = inp.unsqueeze(-2)
+        #print("after unsqueeze, inp_shape",inp.shape)
+
+        
+        H = self.conv_encoder(inp)
+        #print("after encoder, H_shape", H.shape)
+
+        H = H[:,:,:,:self.t]
+
         H = F.pad(H, pad=(0,self.win_size-1,0,0,0,0,0,0), mode='constant', value=0)
         inp_hat = self.conv_decoder(H).squeeze(dim=-2)
+        #print("inp_hat", inp_hat.shape)
 
         
         return inp, inp_hat
