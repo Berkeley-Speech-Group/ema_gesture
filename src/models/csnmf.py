4d96141a03672db16acb1964b549dd828d22c702
jlian2 <jlian2@jlian2s-MacBook-Pro.local>
2021-11-09 23:02:41 -0800

    add ae_csnmf decoder

--- src/models/csnmf.py
+++ src/models/csnmf.py
@@ -5,7 +5,6 @@ import torch.nn.functional as F
 class CSNMF(nn.Module):
     def __init__(self):
         super().__init__()
-
         #shape of X is [1, t, 12]
         self.win_size = 10
         self.t = 500
@@ -22,5 +21,29 @@ class CSNMF(nn.Module):
         H_unfold = H_unfold[:self.win_size]
         H = torch.flip(H_unfold, dims=[0]) #[win_size, num_gestures, t]
         inp_hat = torch.matmul(self.W.transpose(0,1), H).sum(dim=0) #[num_pellets, t]
+        
+        return inp, inp_hat
+
+class AE_CSNMF(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        #shape of X is [1, t, 12]
+        self.win_size = 10
+        self.t = 500
+        self.num_pellets = 12
+        self.num_gestures = 100
+        self.conv_decoder = nn.Conv2d(in_channels=1,out_channels=self.num_pellets,kernel_size=(self.num_gestures,self.win_size),padding=0)
+
+
+    def forward(self, inp):
+        #inp shape is [B,t,num_pellets]
+        inp = inp.transpose(-1, -2) #[B, num_pellets, t]
+        batch_size = inp.shape[0]
+
+        H = torch.randn(batch_size,1,self.num_gestures, self.t)
+        H = F.pad(H, pad=(0,self.win_size-1,0,0,0,0,0,0), mode='constant', value=0)
+        inp_hat = self.conv_decoder(H).squeeze(dim=-2)
+
         
         return inp, inp_hat
