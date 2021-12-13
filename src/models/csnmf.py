5c9415a30357cde51b33b57190e70c305ec7a003
System Administrator <root@jlian2s-MacBook-Pro.local>
2021-12-13 14:30:18 -0800

    clean code

--- src/models/csnmf.py
+++ src/models/csnmf.py
@@ -5,83 +5,7 @@ import numpy as np
 import math
 from utils import get_sparsity
 
-class NegativeClipper(object):
 
-    def __init__(self, frequency=5):
-        self.frequency = frequency
-    def __call__(self, module):
-        # filter the variables to get the ones you want
-        if hasattr(module, 'weight'):
-            w = module.weight.data
-            w = w.clamp(0, torch.max(w))
-            module.weight.data = w
-
-#The Vanilla CNMF model is only able to train the huge kinematics signal
-class CNMF(nn.Module):
-    def __init__(self, **args):
-        super().__init__()
-        #shape of X is [1, t, 12]
-        self.win_size = args['win_suze']
-        self.t = 500
-        self.num_pellets = 12
-        self.num_gestures = 100
-        self.W = nn.Parameter(torch.randn(self.num_pellets, self.win_size, self.num_gestures)*0.01)#[num_pellets, win_size, num_gestures]
-        self.H = nn.Parameter(torch.randn(self.num_gestures, self.t)*0.01)#[num_gestures, t]
-
-    def forward(self, inp):
-        #inp shape is [B,t,num_pellets]
-        H_pad = F.pad(self.H, pad=(self.win_size-1,0,0,0), mode='constant', value=0)
-        H_unfold = H_pad.unfold(1,self.t,1)
-        H_unfold = H_unfold.transpose(0,1)
-        H_unfold = H_unfold[:self.win_size]
-        H = torch.flip(H_unfold, dims=[0]) #[win_size, num_gestures, t]
-        inp_hat = torch.matmul(self.W.transpose(0,1), H).sum(dim=0) #[num_pellets, t]
-        
-        
-        return inp, inp_hat
-
-class AE_CNMF(nn.Module):
-    def __init__(self,**args):
-        super().__init__()
-
-        self.win_size = args['win_size']
-        self.t = args['segment_len']
-        self.num_pellets = args['num_pellets']
-        self.num_gestures = args['num_gestures']
-        self.conv_encoder = nn.ConvTranspose2d(in_channels=self.num_pellets,out_channels=1,kernel_size=(self.num_gestures,self.win_size),padding=0)
-        self.conv_decoder = nn.Conv2d(in_channels=1,out_channels=self.num_pellets,kernel_size=(self.num_gestures,self.win_size),padding=0)
-
-    def forward(self, x):
-        #shape of x is [B,t,num_pellets]
-        #print("max of x", torch.max(x))
-        #print("min of x", torch.min(x))
-        x = x.transpose(-1, -2) #[B, num_pellets, t]
-        #print("before unsqueeze, inp_shape",inp.shape)
-        inp = x.unsqueeze(-2)
-        #print("after unsqueeze, inp_shape",inp.shape)
-        H = F.relu(self.conv_encoder(inp)) #[B, 1, num_gestures, num_points]
-        #print("after encoder, H_shape", H.shape)
-        H = H[:,:,:,:x.shape[2]] #The segment length should be the same as input sequence during testing
-        H = F.pad(H, pad=(0,self.win_size-1,0,0,0,0,0,0), mode='constant', value=0)
-        inp_hat = self.conv_decoder(H).squeeze(dim=-2) #[B, ]
-        return x, inp_hat
-
-    def loadParameters(self, path):
-        self_state = self.state_dict()
-        loaded_state = torch.load(path)
-
-        for name, param in loaded_state.items():
-            origname = name
-            if name not in self_state:
-                name = name.replace("module.", "")
-                if name not in self_state:
-                    print("%s is not in the model."%origname)
-                    continue
-            if self_state[name].size() != loaded_state[origname].size():
-                print("Wrong parameter length: %s, model: %s, loaded: %s"%(origname, self_state[name].size(), loaded_state[origname].size()));
-                continue
-        
-            self_state[name].copy_(param)
 
 class AE_CSNMF(nn.Module):
     def __init__(self, **args):
@@ -279,7 +203,6 @@ class AE_CSNMF2(nn.Module):
             v = v + alphap * w
         
             mask = v < 0
-            
             #print("mask$$$$", mask)
             
             if not torch.any(mask):
