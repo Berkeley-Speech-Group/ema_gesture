804a19d2c9b1b5cabe1e87e63e06212c49fc76fe
root <root@ip-172-31-11-245.ec2.internal>
2022-02-18 12:08:06 +0000

    ieee2meta

---
+++ src/preprocess/ieee2meta.py
@@ -0,0 +1,41 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+path = 'data/ieee'
+
+all_ema = []
+
+meta_list = []
+
+for spk_id in tqdm(os.listdir(path)):
+    if not "F01" in spk_id:
+        continue
+    if not spk_id.startswith("F") and not spk_id.startswith("M"):
+        continue
+    spk_id_path = os.path.join(path, spk_id)
+    spk_id_path = os.path.join(spk_id_path, 'data')
+    for npy_file in tqdm(os.listdir(spk_id_path)):
+        if not npy_file.endswith(".npy"):
+            continue
+        npy_id = npy_file[:-4]
+        npy_path = os.path.join(spk_id_path, npy_file)
+        meta_list.append(npy_path)
+
+data_size = len(meta_list)
+meta_list_train = meta_list[:int(0.8*data_size)]
+meta_list_test = meta_list[int(0.8*data_size):]
+        
+with open("data/ieee/train_metalist_F0.txt", 'w') as f:
+    for i in range(len(meta_list_train)):
+        f.write(meta_list_train[i])
+        if i != len(meta_list_train) - 1:
+            f.write("\n")
+            
+with open("data/ieee/test_metalist_F0.txt", 'w') as f:
+    for i in range(len(meta_list_test)):
+        f.write(meta_list_test[i])
+        if i != len(meta_list_test) - 1:
+            f.write("\n")
+
---
+++ src/preprocess/normalize_ieee_npy.py
@@ -0,0 +1,51 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+path = 'data/ieee'
+
+all_ema = []
+
+for spk_id in tqdm(os.listdir(path)):
+    if not spk_id.startswith("F") and not spk_id.startswith("M"):
+        continue
+    spk_id_path = os.path.join(path, spk_id)
+    spk_id_path = os.path.join(spk_id_path, 'data')
+    for npy_file in tqdm(os.listdir(spk_id_path)):
+        if not npy_file.endswith(".npy"):
+            continue
+        npy_id = npy_file[:-4]
+        npy_path = os.path.join(spk_id_path, npy_file)
+        ema_data = np.load(npy_path) #[T, 24]
+        all_ema.append(ema_data)
+
+all_ema = np.concatenate(all_ema, axis=0) #[1421115, 24]
+means = np.mean(all_ema, axis=0).reshape(1, -1) #[1, 24]
+stds = np.std(all_ema, axis=0).reshape(1, -1) #[1, 24]
+
+
+
+np.save(os.path.join(path, "mean"), means)
+np.save(os.path.join(path, "stds"), stds)
+
+
+print("Finished Means and Stds")
+
+for spk_id in tqdm(os.listdir(path)):
+    if not spk_id.startswith("F") and not spk_id.startswith("M"):
+        continue
+    spk_id_path = os.path.join(path, spk_id)
+    spk_id_path = os.path.join(spk_id_path, 'data')
+    for npy_file in tqdm(os.listdir(spk_id_path)):
+        if not npy_file.endswith(".npy"):
+            continue
+        npy_id = npy_file[:-4]
+        npy_path = os.path.join(spk_id_path, npy_file)
+        ema_data = np.load(npy_path) #[T, 24]
+        
+        ema_data_norm = (ema_data - means) / stds
+        
+        np.save(npy_path[:-4], ema_data_norm)
+        
+print("Norm finished")
